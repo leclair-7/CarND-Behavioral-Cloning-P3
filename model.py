fc06d4d0349fcc203c29fc30607ed61319e641f0
Lucas Hagel <wolf_leclair7@tutanota.com>
2018-03-31 22:20:38 -0500

    Put elu activation functions because they seem to deal better with negative numbers, resized images, simplified the model

--- model.py
+++ model.py
@@ -1,6 +1,17 @@
+
+#Commandline input if we want a histogram of steering angles
+import sys
+
 import cv2
 import numpy as np
 import csv
+
+EPOCHS = 7
+STEER_CORRECTION = .2
+
+#starts out at 160 x 320
+NEW_HEIGHT = 120
+NEW_WIDTH = 240
 
 lines = []
 
@@ -20,6 +31,7 @@ for line in lines:
 	righImage_path = line[2].split('\\')[-1]
 	
 	#current_path = './data/IMG/' + filename
+	#cv2.imread reads it in as BGR...
 	center_image = cv2.imread('./data/IMG/' + centImage_path)
 	left_image   = cv2.imread('./data/IMG/' + leftImage_path)
 	right_image  = cv2.imread('./data/IMG/' + righImage_path)
@@ -28,52 +40,70 @@ for line in lines:
 	images.extend((center_image, left_image, right_image, image_flipped))	
 	
 	steering_angle = float(line[3])
+	
 	#add a correction var?
 	steering_angle_flipped = -steering_angle
-	steering_angles.extend( (steering_angle,steering_angle,steering_angle,steering_angle_flipped) )
-
+	left_steering_angle = steering_angle + STEER_CORRECTION
+	right_steering_angle = steering_angle - STEER_CORRECTION
+	steering_angles.extend( (steering_angle,left_steering_angle,right_steering_angle,steering_angle_flipped) )
+image = [cv2.cvtColor(i, cv2.COLOR_BGR2RGB) for i in images]
 X_train = np.array(images)
 y_train = np.array(steering_angles)
 
-
+'''
+from matplotlib import pyplot
+x = np.arange(-1.5, 1.5, .02)
+pyplot.hist(y_train,x)
+pyplot.title('Steering Angles Histogram')
+pyplot.show()
+'''
 '''
 Since this is a regression model minimize distance between how we drove the car
 and what this model does 
-
 '''
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D
+from keras.backend import tf as ktf
+'''
+'''
+images = [ktf.image.resize_images(image, (NEW_HEIGHT, NEW_WIDTH) ) for image in images]
+
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.5 - 0.5, input_shape=(160, 320, 3)))
 
 model.add(Cropping2D(((50,20),(0,0)), input_shape=(160,320,3)))
 
-model.add(Conv2D(24, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu'))
+#model.add(Lambda(lambda x: my_resize_function(x)))
 
-model.add(Conv2D(36, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu'  ))
 
-model.add(Conv2D(48, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu'))
+model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2),
+                 activation='elu'))
+
+model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2),
+                 activation='elu'  ))
+
+'''
+model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2),
+                 activation='elu'))
 
 model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2),
-                 activation='relu'))
+                 activation='elu'))
+'''
 model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-                 activation='relu'))
+                 activation='elu'))
 model.add(Flatten())
-#model.add(Dense(1124, activation='relu'))
+#model.add(Dense(1124, activation='elu'))
 
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(100, activation='elu'))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
 
 #output the steering angle
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=.2,shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=.2,shuffle=True, epochs=EPOCHS)
 
 model.save('model.h5')
 
