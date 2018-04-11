c0519f19e2e09aa676f923c48d7ff261ff57bb44
Lucas Hagel <wolf_leclair7@tutanota.com>
2018-04-10 22:25:32 -0500

    revamped the data input, it works ~5x faster than the last one, took out redundant function calls, and some useless calculations

--- get_data_in.py
+++ get_data_in.py
@@ -32,11 +32,8 @@ BATCH_SIZE = 128
 EPOCHS = 3
 ACTIVATION_FUNCTION = 'elu'
 
-data = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        data.append(line)
+
+
 def flipImages(images, steering_angle):
     flipped = np.copy(np.fliplr(images))   
     return (flipped, -1 * steering_angle)
@@ -54,128 +51,127 @@ def change_brightness(image):
 
 def preprocess_image(image):
     '''
-    crop
-    resize 200 across 66 up/down
-    convert to YUV
+    1 crop
+    2 resize 200 across 66 up/down
+    3 convert to YUV
     '''
-    print(image.shape)
     x,w = 25,image.shape[1]-25    
     #left to cut off, right to cut off
     y,h = 65, (image.shape[0] - 90)
     image = np.copy(image[y:y+h, x:x+w])
-    print(image.shape)
-    porky=input()
+
+    #print(image.shape)
     image = cv2.resize(image, (200,66) )
     return cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
 
-train_samples, validation_samples = train_test_split(data, test_size=0.2)
 
-def generator(samples, batch_size=BATCH_SIZE):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        samples = shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
+def generator(samples, tags, batch_size=BATCH_SIZE):
+    
+    samples, tags = shuffle(samples, tags)
 
+    assert len(samples) == len(tags)
+    
+    while 1:
+
+        for offset in range(0, len(samples), batch_size):
+
+            batch_samples, batch_tags = samples[offset:offset+batch_size], tags[offset:offset+batch_size]
+            
             images = []
             steering_angles = []
-            for batch_sample in batch_samples:
-                name = './data/IMG/'+ batch_sample[0].split('/')[-1]
-                left = './data/IMG/'+ batch_sample[1].split('/')[-1]
-                right = './data/IMG/'+ batch_sample[2].split('/')[-1]
-                
-                #print(len(images), name)
-                if name == "./data/IMG/center":
-                    continue
-                    
-                center_image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
-                left_image  = cv2.cvtColor(cv2.imread(left), cv2.COLOR_BGR2RGB)
-                right_image = cv2.cvtColor(cv2.imread(right), cv2.COLOR_BGR2RGB)
 
-                #steering angles
-                center_angle = float(batch_sample[3])
-                left_angle = float(batch_sample[3]) + STEER_CORRECTION
-                right_angle = float(batch_sample[3]) - STEER_CORRECTION
+            for bs in range(len(batch_samples)):
+                '''
+                print(batch_sample)
+                p=input()
+                '''
+                an_image = cv2.cvtColor(cv2.imread(batch_samples[bs]), cv2.COLOR_BGR2RGB)
                 
-                coinFlip = random.random()
-                if isclose(center_angle, 0.0):
-                    if coinFlip > .7:
-                        images.extend( (center_image,left_image, right_image ) )
-                        steering_angles.extend( (center_angle, left_angle, right_angle) )
-                else:
-                    images.extend( (center_image,left_image, right_image ) )
-                    steering_angles.extend( (center_angle, left_angle, right_angle) )
+                images.append(  an_image  )
+                steering_angles.append( batch_tags[bs] )
+                
+                if len(images) >= BATCH_SIZE:
+                    break
+
                 if coinFlip < PERCENTAGE_FLIPPED:
-                    flipcenter = flipImages(center_image,center_angle)
-                    flipleft = flipImages(left_image,left_angle)
-                    flipright = flipImages(right_image,right_angle)
+                    flipped_image,flipped_angle = flipImages(an_image,batch_tags[bs] )                    
+                    
+                    images.append( flipped_image )
+                    steering_angles.append( flipped_angle )
+                
+                if len(images) >= BATCH_SIZE:
+                    break
 
-                    images.append(flipcenter[0])
-                    steering_angles.append(flipcenter[1])
-
-                    images.append(flipleft[0])
-                    steering_angles.append(flipleft[1])
-
-                    images.append(flipright[0])
-                    steering_angles.append(flipright[1])
                 if coinFlip < BRIGHTNESS_CHANGES:
-                    img_left_bright = change_brightness(left_image)
-                    img_right_bright = change_brightness(right_image)
+                    brightness_changed_image = change_brightness(an_image)                    
+                    images.append( brightness_changed_image  )
+                    steering_angles.append( batch_tags[bs] )
+            
+            images = np.array(images)
+            steering_angles = np.array(steering_angles)
 
-                    images.append(img_left_bright)
-                    steering_angles.append(left_angle)
+            if len(images) >= BATCH_SIZE:
+                yield images[:BATCH_SIZE], steering_angles[:BATCH_SIZE]
+                samples, tags = shuffle(samples, tags)
 
-                    images.append(img_right_bright)
-                    steering_angles.append(right_angle)
-            X_train = np.array(images)
-            y_train = np.array(steering_angles)
+data = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        data.append(line)
 
-            X_train, y_train = shuffle(X_train, y_train)
-            yield X_train[:BATCH_SIZE], y_train[:BATCH_SIZE]
+image_paths = []
+steering_angles = []
 
-train_generator = generator(train_samples, batch_size=BATCH_SIZE)
-validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
+for img_file_label in data:
+    center = './data/IMG/'+ img_file_label[0].split('/')[-1]
+    left = './data/IMG/'+ img_file_label[1].split('/')[-1]
+    right = './data/IMG/'+ img_file_label[2].split('/')[-1]
 
-def my_resize_function(images):
-    from keras.backend import tf as ktf
-    return ktf.image.resize_images(images, (NEW_WIDTH,NEW_HEIGHT))
+    if center == "./data/IMG/center":
+        continue
+
+    center_angle = float(img_file_label[3])
+    left_angle = float(img_file_label[3]) + STEER_CORRECTION
+    right_angle = float(img_file_label[3]) - STEER_CORRECTION
+
+    #steering angles
+    coinFlip = random.random()
+    if isclose(center_angle, 0.0):
+        if coinFlip > .7:
+            image_paths.extend( (center,left, right ) )
+            steering_angles.extend( (center_angle, left_angle, right_angle) )
+    else:
+        image_paths.extend( (center,left, right ) )
+        steering_angles.extend( (center_angle, left_angle, right_angle) )
+    
+image_paths = np.array(image_paths)
+steering_angles = np.array(steering_angles)
+
+
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+train_samples, test_samples, train_angles, test_angles = train_test_split(image_paths, steering_angles, test_size=0.2, random_state=42)
+
+train_generator = generator(train_samples, train_angles, batch_size=BATCH_SIZE)
+validation_generator = generator(test_samples, test_angles, batch_size=BATCH_SIZE)
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.5 - 0.5, input_shape=(height, width, 3)))
 
-model.add(Cropping2D(((70,25),(0,0)), input_shape=(height,width,3)))
+model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation=ACTIVATION_FUNCTION))
+model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation=ACTIVATION_FUNCTION  ))
+model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation=ACTIVATION_FUNCTION))
 
-#model.add(Lambda(lambda x: cv2.resize(x, (NEW_HEIGHT, NEW_WIDTH)) ) )
-#, input_shape=(num_channels, height, width), output_shape=(ch, new_height, new_width))
+model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2), activation=ACTIVATION_FUNCTION))
+model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2), activation=ACTIVATION_FUNCTION))
 
-#model.add(Lambda(lambda x: ImageDataGenerator(x, )))
-#model.add(Lambda(lambda image: tf.image.resize_images(image, (NEW_HEIGHT, NEW_WIDTH))))
-model.add(Lambda(my_resize_function))
-
-model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2),
-                 activation=ACTIVATION_FUNCTION))
-
-model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2),
-                 activation=ACTIVATION_FUNCTION  ))
-
-model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2),
-                 activation=ACTIVATION_FUNCTION))
-#model.add()
-'''
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2),
-                 activation=ACTIVATION_FUNCTION))
-'''
-'''
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-                 activation=ACTIVATION_FUNCTION))
-'''
 model.add(Flatten())
 
-#model.add(Dense(1184, activation=ACTIVATION_FUNCTION))
 model.add(Dense(100, activation=ACTIVATION_FUNCTION))
 model.add(Dense(50, activation=ACTIVATION_FUNCTION))
 model.add(Dense(10, activation=ACTIVATION_FUNCTION))
 
+#output layer... important !
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
@@ -184,7 +180,7 @@ model.compile(loss='mse', optimizer='adam')
 
 model.fit_generator(train_generator, samples_per_epoch= \
             len(train_samples), validation_data=validation_generator, \
-            nb_val_samples=len(validation_samples), nb_epoch=EPOCHS, steps_per_epoch=len(train_samples)//BATCH_SIZE)
+            nb_val_samples=len(test_samples), nb_epoch=EPOCHS, steps_per_epoch=len(train_samples)//BATCH_SIZE)
 
 model.save('model.h5')
 
